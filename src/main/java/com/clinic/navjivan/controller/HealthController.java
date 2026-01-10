8bb6231540f713b13f67b225e6117f63b4998b22
nimesh <nimeshpatel2050@gmail.com>
2026-01-10 21:00:15 +0530

    set controller

---
+++ src/main/java/com/clinic/navjivan/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.clinic.navjivan.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String health() {
+        return "Navjivan backend is running";
+    }
+}
